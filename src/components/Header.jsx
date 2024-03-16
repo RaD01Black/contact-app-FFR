d04e8dc4ad2747a4d4d06126b124a7a4c50a4f3d
Alereza_RaDBlaCK01 <aliireza01ahmadi@gmail.com>
2024-03-16 02:21:07 +0100

    Making Header part

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import styles from "./Header.module.css"
+
+function Header() {
+  return (
+    <div className={styles.container}>
+        <h1>Contact App</h1>
+        <p>
+            <a href="https://radblack.online/">RaDBlack01</a> | React.js FFR
+        </p>
+    </div>
+  )
+}
+
+export default Header
