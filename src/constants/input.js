a19aec16f2e8f4eda8661e1b15ead8b052eaa628
Alereza_RaDBlaCK01 <aliireza01ahmadi@gmail.com>
2024-03-17 15:45:24 +0100

    Update input.js

--- src/constants/input.js
+++ src/constants/input.js
@@ -2,7 +2,7 @@ const inputs = [
     { type: "text", name: "name", placeholder: "Name"},
     { type: "text", name: "LastName", placeholder: "Last Name"},
     { type: "text", name: "email", placeholder: "Email"},
-    { type: "text", name: "phone", placeholder: "Phone"},
+    { type: "number", name: "phone", placeholder: "Phone"},
 ]
 
 
